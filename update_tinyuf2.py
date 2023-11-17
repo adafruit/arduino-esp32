255fcea2df3ef26ba2f947f0afd78445b41ada1f
hathach <thach@tinyusb.org>
2023-11-17 22:23:33 +0700

    add tinyuf2 no-ota

--- update_tinyuf2.py
+++ update_tinyuf2.py
@@ -4,34 +4,36 @@ import urllib.request
 import zipfile
 from multiprocessing import Pool
 
-version = '0.16.0'
+version = '0.18.1'
 print('version {}'.format(version))
 
 # variant name, tinyuf2 bootloader name
 
-all_variant = [
-    ['adafruit_metro_esp32s3', ''],
-]
-
 # all_variant = [
-#     #  Feather
-#     ['adafruit_feather_esp32s2', ''],
-#     ['adafruit_feather_esp32s2_reversetft', 'adafruit_feather_esp32s2_reverse_tft'],
-#     ['adafruit_feather_esp32s2_tft', ''],
 #     ['adafruit_feather_esp32s3', ''],
-#     ['adafruit_feather_esp32s3_nopsram', ''],
-#     ['adafruit_feather_esp32s3_reversetft', 'adafruit_feather_esp32s3_reverse_tft'],
-#     ['adafruit_feather_esp32s3_tft', ''],
-#     # Funhouse, magtag, metro
-#     ['adafruit_funhouse_esp32s2', ''],
-#     ['adafruit_magtag29_esp32s2', 'adafruit_magtag_29gray'],
-#     ['adafruit_metro_esp32s2', ''],
-#     ['adafruit_metro_esp32s3', ''],
-#     # qt py
-#     ['adafruit_qtpy_esp32s2', ''],
-#     ['adafruit_qtpy_esp32s3_nopsram', 'adafruit_qtpy_esp32s3'],
-#     ['adafruit_qtpy_esp32s3_n4r2', ''],
 # ]
+
+# [variant name, download name]
+# Alphabetical order
+all_variant = [
+    ['adafruit_camera_esp32s3', ''],
+    ['adafruit_feather_esp32s2', ''],
+    ['adafruit_feather_esp32s2_reversetft', 'adafruit_feather_esp32s2_reverse_tft'],
+    ['adafruit_feather_esp32s2_tft', ''],
+    ['adafruit_feather_esp32s3', ''],
+    ['adafruit_feather_esp32s3_nopsram', ''],
+    ['adafruit_feather_esp32s3_reversetft', 'adafruit_feather_esp32s3_reverse_tft'],
+    ['adafruit_feather_esp32s3_tft', ''],
+    ['adafruit_funhouse_esp32s2', ''],
+    ['adafruit_magtag29_esp32s2', 'adafruit_magtag_29gray'],
+    ['adafruit_matrixportal_esp32s3', 'adafruit_matrixportal_s3'],
+    ['adafruit_metro_esp32s2', ''],
+    ['adafruit_metro_esp32s3', ''],
+    ['adafruit_qtpy_esp32s2', ''],
+    ['adafruit_qtpy_esp32s3_n4r2', ''],
+    ['adafruit_qtpy_esp32s3_nopsram', 'adafruit_qtpy_esp32s3'],
+    ['adafruit_qualia_s3_rgb666', ''],
+]
 
 
 def update_variant(v):
@@ -40,7 +42,8 @@ def update_variant(v):
 
     # Download from bootloader release
     name = 'tinyuf2-{}-{}.zip'.format(dl_name, version)
-    url = 'https://github.com/adafruit/tinyuf2/releases/download/{}/tinyuf2-{}-{}.zip'.format(version, dl_name, version)
+    url = 'https://github.com/adafruit/tinyuf2/releases/download/{}/tinyuf2-{}-{}.zip'.format(
+        version, dl_name, version)
     print("Downloading TinyUF2 for", variant)
     urllib.request.urlretrieve(url, variant)
 
@@ -49,12 +52,9 @@ def update_variant(v):
     # unzip (will overwrite old files)
     with zipfile.ZipFile(variant, "r") as zf:
         zf.extract("bootloader.bin", variant_path)
-        os.renames(os.path.join(variant_path, "bootloader.bin"), os.path.join(variant_path, "bootloader-tinyuf2.bin"))
+        os.renames(os.path.join(variant_path, "bootloader.bin"),
+                   os.path.join(variant_path, "bootloader-tinyuf2.bin"))
         zf.extract("tinyuf2.bin", variant_path)
-        for zn in zf.namelist():
-            if zn.endswith('.csv'):
-                zf.extract(zn, variant_path)
-                os.renames(os.path.join(variant_path, zn), os.path.join(variant_path, os.path.splitext(zn)[0] + "-tinyuf2.csv"))
 
     # remove zip file
     os.remove(variant)
@@ -62,4 +62,3 @@ def update_variant(v):
 
 with Pool(processes=os.cpu_count()) as pool:
     pool.map(update_variant, all_variant)
-
